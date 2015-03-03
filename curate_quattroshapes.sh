2b8c9a0547c3ce91acfc31d4c354423d44374d7c
Grant Heffernan <heffergm@gmail.com>
2015-03-03 13:37:22 -0500

    * make process.sh executable for consistency. * run ogr2ogr commands for each given alpha3 in the background then wait for completion before moving to next.

--- curate_quattroshapes.sh
+++ curate_quattroshapes.sh
@@ -20,12 +20,13 @@ mkdir -p quattroshapes-alpha3/compressed
 
 for i in $(cat alpha3.csv); do
   mkdir quattroshapes-alpha3/${i}
-  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_admin0.shp        simplified/qs_adm0.shp
-  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_admin1.shp        simplified/qs_adm1.shp
-  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_admin2.shp        simplified/qs_adm2.shp
-  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_localadmin.shp    simplified/qs_localadmin.shp
-  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_localities.shp    simplified/qs_localities.shp
-  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_neighborhoods.shp simplified/qs_neighborhoods.shp
+  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_admin0.shp        simplified/qs_adm0.shp &
+  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_admin1.shp        simplified/qs_adm1.shp &
+  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_admin2.shp        simplified/qs_adm2.shp &
+  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_localadmin.shp    simplified/qs_localadmin.shp &
+  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_localities.shp    simplified/qs_localities.shp &
+  ogr2ogr -f "ESRI Shapefile" -where "qs_adm0_a3 = '${i}'" quattroshapes-alpha3/${i}/${i}_neighborhoods.shp simplified/qs_neighborhoods.shp &
+  wait
 
   tar czf quattroshapes-alpha3/compressed/${i}.tgz -C quattroshapes-alpha3 ${i}
 done
