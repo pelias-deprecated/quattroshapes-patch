38bace7ba6be211c5210da2b57025909fcbf92b4
Severyn Kozak <severyn.kozak@gmail.com>
2015-03-30 11:49:32 -0400

    Strip verbose UK admin1 prefixes.
    clean_data.sql
    	-Add a query to strip weird UK admin1 prefixes (fix
    	pelias/quattroshapes-pipeline#13).

--- clean_data.sql
+++ clean_data.sql
@@ -3,7 +3,15 @@
 
 -- Some admin2 records have qs_a2/qs_a2_alt names with trailing parentheticals
 -- (like `(c)`, `(a)`); remove those characters.
+-- See pelias/quattroshapes-pipeline#8.
 update qs_adm2
 set
 	qs_a2 = regexp_replace(qs_a2, ' \(.+\)$', '', ''),
 	qs_a2_alt = regexp_replace(qs_a2_alt, ' \(.+\)$', '', '');
+
+-- Some (predominantly UK) admin1 records have qs_a1 names with verbose and
+-- unnecessary prefixes (like 'City of'). Remove them.
+-- See pelias/quattroshapes-pipeline#13.
+update qs_adm1
+set
+	qs_a1 = regexp_replace(qs_a1, '^((the )?city of|city and county of) ', '', 'i');
